e17e9e6a5c231e28bb3d7235fd20b902b121f795
Indrek Mäeots <indrek160@gmail.com>
2023-03-07 10:18:50 +0200

    Create Java project for Words app

---
+++ src/main/java/com/imaeots/wordcounter/WordsMain.java
@@ -0,0 +1,7 @@
+package com.imaeots.wordcounter;
+
+public class WordsMain {
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
---
+++ src/test/java/com/imaeots/wordcounter/MainTest.java
@@ -0,0 +1,12 @@
+package com.imaeots.wordcounter;
+
+import org.junit.jupiter.api.Test;
+import java.io.IOException;
+
+// This is for testing
+public class MainTest {
+    @Test
+    public void testWordsMain() throws IOException {
+        WordsMain.main(new String[]{});
+    }
+}
